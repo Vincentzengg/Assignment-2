e5211837fc4e56ab10f51993cbea188ad2100c51
Vincentzengg <124546793+Vincentzengg@users.noreply.github.com>
2023-05-10 10:09:06 +1000

    syncing

---
+++ Functions.py
@@ -0,0 +1,66 @@
+import pygame
+screen_x = 500
+screen_y = 500
+screen = pygame.display.set_mode((screen_x, screen_y),0,32)
+black = (0, 0, 0)
+white = (255, 255, 255)
+line_length = 100
+line_width = 16
+
+def dot(x_cord, y_cord):
+    pygame.draw.circle(screen, black, (x_cord + line_width/2, y_cord + line_width/2), line_width)
+
+def line(colour, x_cord, y_cord, length, width, border):
+    pygame.draw.rect(screen, black, (x_cord, y_cord, length, width))
+    pygame.draw.rect(screen, colour, (x_cord + border, y_cord + border, length - 2*border, width- 2*border))
+
+def Grid(gridSizeX, gridSizeY):
+
+    x= 10
+    y= 10
+    for i in range(0, gridSizeY):
+        for i in range(0, gridSizeX):
+    
+            line(white, x, y, line_length, line_width, 2)
+            line(white, x, y, line_width, line_length, 2)
+            dot(x, y)
+            x += line_length
+        x = 10
+        y += line_length
+
+    y= 10
+    x = 10
+
+    for i in range(0, gridSizeY):
+
+        line(white, x + (line_length * (gridSizeX)), y, line_width, line_length, 2)
+        dot(x + (line_length * (gridSizeX)), y)
+        
+        y += line_length
+    x = 10
+    y = 10
+    for i in range(0, gridSizeX):
+     
+        line(white, x , y + (line_length * (gridSizeY)), line_length, line_width, 2)
+        dot(x, y + (line_length * (gridSizeY)))
+        x += line_length
+    x = 10
+    y = 10
+    dot(x + (line_length * gridSizeX), y + (line_length *gridSizeY))
+
+
+class Button():
+    
+    def __init__(self, x, y) -> None:
+
+        width = line_length
+        height = line_width
+        self.image = line(white, x, y, 100, 16, 2)
+
+    def draw():
+        pos = pygame.mouse.get_pos      
+        
+    
+        
+
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+from Functions import *
 import sys
 #VARIABLES
 screen_x = 800
@@ -7,8 +8,8 @@ screen_y = 800
 white = (255, 255, 255)
 blue = (0, 0, 255)
 black = (0, 0, 0)
-line_length = 50
-line_width = 10
+red = (255, 0, 0)
+
 x = 10
 y = 10
 fps = 60
@@ -18,62 +19,16 @@ screen.fill(white)
 
 objects = []
 
-def line(colour, x_cord, y_cord, length, width, border):
-    pygame.draw.rect(screen, black, (x_cord, y_cord, length, width))
-    pygame.draw.rect(screen, colour, (x_cord + border, y_cord + border, length - 2*border, width- 2*border))
 
-def dot(x_cord, y_cord):
-    pygame.draw.circle(screen, black, (x_cord + line_width/2, y_cord + line_width/2), line_width)
-    
-
-
-def Grid(gridSizeX, gridSizeY):
-    x= 10
-    y= 10
-    for i in range(0, gridSizeY):
-        for i in range(0, gridSizeX):
-    
-            line(white, x, y, line_length, line_width, 2)
-            line(white, x, y, line_width, line_length, 2)
-            dot(x, y)
-            x += line_length
-        x = 10
-        y += line_length
-
-    y= 10
-    x = 10
-
-    for i in range(0, gridSizeY):
-
-        line(white, x + (line_length * (gridSizeX)), y, line_width, line_length, 2)
-        dot(x + (line_length * (gridSizeX)), y)
-        
-        y += line_length
-    x = 10
-    y = 10
-    for i in range(0, gridSizeX):
-     
-        line(white, x , y + (line_length * (gridSizeY)), line_length, line_width, 2)
-        dot(x, y + (line_length * (gridSizeY)))
-        x += line_length
-    x = 10
-    y = 10
-    dot(x + (line_length * gridSizeX), y + (line_length *gridSizeY))
 Grid(5, 5)
-
-
 
 def main():
      
     while True:
         
-
         
 
 
-        
-
-        
 
         for event in pygame.event.get():
             if event.type==QUIT:
@@ -85,6 +40,8 @@ def main():
             object.process()
         pygame.display.flip()
         fpsClock.tick(fps)
+
+        Button(300, 400)
 
 
 main()
