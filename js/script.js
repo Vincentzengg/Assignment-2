e3eef83ca0eea490ad7d0c4d79f256337d553659
Vincentzengg <124546793+Vincentzengg@users.noreply.github.com>
2023-05-28 19:10:44 +1000

    sdasda

--- js/script.js
+++ js/script.js
@@ -12,8 +12,8 @@ function load() {
     var m = 10;
     var offset = 50;
 
-    var screen_x = window.innerWidth/2 - (m*offset)/2;
-    screen_y = offset *2.5;
+    var screen_x = window.innerWidth / 2 - (m * offset) / 2;
+    screen_y = offset * 2.5;
     var html = "";
     $("#app").html(html);
     var c = 0;
@@ -24,10 +24,10 @@ function load() {
                 y = screen_y + (j * offset);
 
             html += `
-            <div class ="box" data-id="${c}" style="z-index=${i-1}; left:${x+2.5}px; top:${y+2.5}px"></div>
-            <div class ="dot" style ="z-index=${i}; left:${x-5}px; top:${y-5}px" data-box="${c}"></div>
-            <div class ="line lineh" data-line1="${c}" data-line2="${c-m}" style="z-index=${i}; left:${x}px; top:${y}px" data-active="false"></div>
-            <div class ="line linev" data-line1="${c}" data-line2="${c-1}" style="z-index=${i}; left:${x}px; top:${y}px" data-active="false"></div>
+            <div class ="box" data-id="${c}" style="z-index=${i - 1}; left:${x + 2.5}px; top:${y + 2.5}px"></div>
+            <div class ="dot" style ="z-index=${i}; left:${x - 5}px; top:${y - 5}px" data-box="${c}"></div>
+            <div class ="line lineh" data-line-1="${c}" data-line-2="${c - m}" style="z-index=${i}; left:${x}px; top:${y}px" data-active="false"></div>
+            <div class ="line linev" data-line-1="${c}" data-line-2="${c - 1}" style="z-index=${i}; left:${x}px; top:${y}px" data-active="false"></div>
             `
 
             boxes.push(0);
@@ -41,25 +41,25 @@ function load() {
             y = screen_y + (i * offset);
 
         html += `
-            <div  class="dot" style="z-index=${i}; left:${x-5}px; top:${y-5}px" data-box="${c}"></div>
-            <div class="line linev" data-line-1="${m*(i+1)-1}" data-line-2="${-1}" style="z-index=${i}; left:${x}px; top:${y}px" data-active="false"></div>
+            <div  class="dot" style="z-index=${i}; left:${x - 5}px; top:${y - 5}px" data-box="${c}"></div>
+            <div class="line linev" data-line-1="${m * (i + 1) - 1}" data-line-2="${-1}" style="z-index=${i}; left:${x}px; top:${y}px" data-active="false"></div>
         `
-    }
+    };
 
 
     //bottom box
     for (var i = 0; i < m; i++) {
         var x = screen_x + (i * offset);
-        var  y = screen_y + (n * offset);
+        var y = screen_y + (n * offset);
         html += `
 
-            <div class="dot" style="z-index=${i}; left:${x-5}px; top:${y-5}px" data-box="${c}"></div>
-            <div class="line lineh" data-line-1="${m*(n-1)+i}" data-line-2="${-1}" style="z-index=${i}; left:${x}px; top:${y}px" data-active="false"></div>
+            <div class="dot" style="z-index=${i}; left:${x - 5}px; top:${y - 5}px" data-box="${c}"></div>
+            <div class="line lineh" data-line-1="${m * (n - 1) + i}" data-line-2="${-1}" style="z-index=${i}; left:${x}px; top:${y}px" data-active="false"></div>
             `;
 
 
-    } 
-    html += `<div class="dot" style="z-index=${i}; left:${screen_x+m*offset-5}px; top:${screen_y+n*offset-5}px" data-active="false"></div>
+    }
+    html += `<div class="dot" style="z-index=${i}; left:${screen_x + m * offset - 5}px; top:${screen_y + n * offset - 5}px" data-active="false"></div>
     `
 
     $("#app").html(html);
@@ -67,7 +67,7 @@ function load() {
 
 
 
-    }
+}
 
 function applyEvents() {
     $("div.line").unbind('click').bind('click', function () {
@@ -83,9 +83,9 @@ function applyEvents() {
             $(this).attr("data-active", "true");
 
             if (a === false && b === false) {
-                computer();
-
-
+                turn = false;
+                $("#turn").text("Turn: Computer");
+                Computer();
 
             }
 
@@ -98,21 +98,21 @@ function acquire(id) {
 
     var color;
     if (turn) {
-        color = "salon";
+        color = " ";
         you++;
     }
     else {
         color = "skyblue";
         computer++;
     }
-    $("div.box[data-id='"+id+"']").css("background-color", color);
+    $("div.box[data-id='" + id + "']").css("background-color", color);
     boxes[id] = "full";
 
-    $(".player2").text("You: " + you);
+    $(".player2").text("Player: " + you);
     $(".player1").text("Computer: " + computer);
 
     var fulll = true
-    for (var i = boxes.length-1; i>=0; i--) {
+    for (var i = boxes.length - 1; i >= 0; i--) {
         if (boxes[i] != fulll) {
             fulll = false;
             break;
@@ -137,17 +137,15 @@ function checkValid(t) {
     return ($(t).attr("data-active") === "false");
 
 }
-function computer() {
+function Computer() {
     turn = false;
-    $("#turn").text("Computer's turn");
+    $("#turn").text("Turn: Computer");
 
     setTimeout(function () {
-
         var length = boxes.length;
-
         var arr3 = [], arr2 = [], arr1 = [], arr0 = [];
 
-        for (var i =length-1; i >= 0; i--) {
+        for (var i = length - 1; i >= 0; i--) {
             if (boxes[i] === 3) arr3.push(i);
             else if (boxes[i] === 2) arr2.push(i);
             else if (boxes[i] === 1) arr1.push(i);
@@ -155,21 +153,16 @@ function computer() {
         }
 
         if (arr3.length > 0) {
-
-            compterSelect(arr[random(0, arr3.length - 1)]);
+            computerSelect(arr3[random(0, arr3.length - 1)]);
         }
-
         else if (arr1.length > 0) {
             computerSelect(arr1[random(0, arr1.length - 1)]);
         }
-
         else if (arr0.length > 0) {
             computerSelect(arr0[random(0, arr0.length - 1)]);
         }
-
         else if (arr2.length > 0) {
             computerSelect(arr2[random(0, arr2.length - 1)]);
-
         }
 
     }, 500);
@@ -183,30 +176,29 @@ function selectBox() {
 }
 
 function computerSelect(id) {
-
     console.log("box" + id);
-
     $("div.line[data-line-1='" + id + "'], div.line[data-line-2='" + id + "']").each(function (i, v) {
-
-        if (!$(v).hasClasss("line-active")) {
+        if (!$(v).hasClass("line-active")) {
             var id1 = parseInt($(v).attr("data-line-1"));
             var id2 = parseInt($(v).attr("data-line-2"));
 
-            console.log("-----" + turn)
-
-            if (checkValid(v) && turn === false)
-                console.log("-----");
-                if(id1 >= 0) var a=addValue(id1);
-                if(id2 >= 0) var b=addValue(id2);
-            if (a === true || b === true) {
-                computer();
-
-            } else
-                turn = true;
-            $("#turn").text("Turn :" + "You")
+            if (checkValid(v) && !turn) {
+                var a = false, b = false;
+                if (id1 >= 0) a = addValue(id1);
+                if (id2 >= 0) b = addValue(id2);
+                
+                if (a === true || b === true) {
+                    Computer();
+                } else 
+                    turn = true;
+                    $("#turn").text("Turn: You");
+                
+            }
         }
     });
 }
+
+
 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
