f9d37c5e0d8b59ce2d4bc6a5b990551dd1396040
Vincentzengg <124546793+Vincentzengg@users.noreply.github.com>
2023-06-19 22:00:45 +1000

    sfasf

---
+++ Testing/src/App.jsx
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function GenerateRow(squaresPerRow) {
+  let squareValue = 0
+  const row = [];
+  for (let i = 0; i < squaresPerRow; i++) {
+    row.push(<Square value={squareValue} onSquareClick={() => handleClick(squareValue)} />);
+    squareValue++;
+  }
+
+  return row;
+}
+
+function GenerateBoard(rows, squaresPerRow) {
+
+  const board = [];
+  for (let i = 0; i < rows; i++) {
+    board.push(GenerateRow(squaresPerRow));
+  }
+
+  return board;
+}
+
+
+function Square({value, onSquareClick}) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      
+      
+    </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
