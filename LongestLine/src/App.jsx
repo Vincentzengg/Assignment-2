9d7403a4b90acca15bcdbb2b650195df7b38d4bd
Vincentzengg <124546793+Vincentzengg@users.noreply.github.com>
2023-07-15 23:40:06 +1000

    got half of it setup

--- LongestLine/src/App.jsx
+++ LongestLine/src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   
 
   return (
-    <h1 className="text-3xl underline">
-      Hello world!
-    </h1>
+    <div>
+
+<Header />
+
+    </div>
+    
   )
 }
 
