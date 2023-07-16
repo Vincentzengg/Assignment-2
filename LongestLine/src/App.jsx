f0a50e253bfe26c9e85b0f1aa461c2ade92a6e89
Vincentzengg <124546793+Vincentzengg@users.noreply.github.com>
2023-07-16 12:52:36 +1000

    MADE MULTIPLE WEBPAGES

--- LongestLine/src/App.jsx
+++ LongestLine/src/App.jsx
@@ -1,17 +1,27 @@
 
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
 import Header from './components/Header'
+import About from './pages/About'
+import Settings from './pages/Settings'
 
 function App() {
   
 
   return (
     <div>
-
-<Header />
-
+      <Router>  
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path ="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/settings" element={<Settings />} />
+            
+          </Routes>
+      </Router>
     </div>
-    
   )
 }
 
--- LongestLine/src/components/Header.jsx
+++ LongestLine/src/components/Header.jsx
@@ -18,7 +18,7 @@ export default function Header() {
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-8xl px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-8xl px-0px sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
