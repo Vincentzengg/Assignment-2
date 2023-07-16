0bbda2f011d7d343ffc27d263c24ebf42191c102
Vincentzengg <124546793+Vincentzengg@users.noreply.github.com>
2023-07-16 14:26:46 +1000

    EDIT BUTTON LATER

--- LongestLine/src/pages/Home.jsx
+++ LongestLine/src/pages/Home.jsx
@@ -1,9 +1,28 @@
+import "./Home.css"
+
+
 const Home = () => {
     return (
-        <div class="font-mono">Home</div>
-
+        <>
+        <p id='head1' class='absolute top-1/3 left-1/2 -translate-x-1/2 -translate-x-1/2 text-white font-sans text-4xl font-weight: 200'>Software Assignment 2</p>
+        <p id='head2' class='absolute top-1/3 left-1/2 -translate-x-1/2 -translate-x-1/2 text-white font-sans text-4xl font-weight: 200'>Longest Line game 
+        <p> By Vincent Zeng</p>
+            </p>
+        
+        <button id="myButton" class="" >Play</button>
+        
+        <div class='light x1'></div>
+        <div class='light x2'></div>
+        <div class='light x3'></div>
+        <div class='light x4'></div>
+        <div class='light x5'></div>
+        <div class='light x6'></div>
+        <div class='light x7'></div>
+        <div class='light x8'></div>
+        <div class='light x9'></div>
         
 
+        </>    
     )
 
 }
--- LongestLine/src/pages/PlayerVsPlayer.jsx
+++ LongestLine/src/pages/PlayerVsPlayer.jsx
@@ -124,6 +124,16 @@ export default function PlayerVsPlayer() {
   return ( //All of this here, is to essentially put everything onto the screen, like the board, title, etc.
     <>
 
+        <div class='light x1'></div>
+        <div class='light x2'></div>
+        <div class='light x3'></div>
+        <div class='light x4'></div>
+        <div class='light x5'></div>
+        <div class='light x6'></div>
+        <div class='light x7'></div>
+        <div class='light x8'></div>
+        <div class='light x9'></div>
+
       <h1 className="text-5xl font-bold underline text-slate-500">
         Longest Line Game
       </h1>
