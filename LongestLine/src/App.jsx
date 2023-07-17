5ca4a77c13bac70d4b45eff171e3e1ce2cca86bf
Vincentzengg <124546793+Vincentzengg@users.noreply.github.com>
2023-07-17 11:44:22 +1000

    ss

--- LongestLine/src/App.jsx
+++ LongestLine/src/App.jsx
@@ -2,11 +2,12 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
-import Header from './components/Header'
+
 import About from './pages/About'
 import Settings from './pages/Settings'
 import PlayerVsPlayer from './pages/PlayerVsPlayer'
-
+import EasyBot from './pages/EasyBot'
+import HardBot from './pages/HardBot'
 
 function App() {
   
@@ -14,13 +15,15 @@ function App() {
   return (
     <div>
       <Router>  
-        <Header />
+        
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path ="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/settings" element={<Settings />} />
             <Route path="/PlayerVsPlayer" element={<PlayerVsPlayer />} />
+            <Route path="/EasyBot" element={<EasyBot />} />
+            <Route path="/HardBot" element={<HardBot />} />
           </Routes>
       </Router>
     </div>
--- LongestLine/src/pages/Home.jsx
+++ LongestLine/src/pages/Home.jsx
@@ -4,12 +4,31 @@ import "./Home.css"
 const Home = () => {
     return (
         <>
-        <p id='head1' class='absolute top-1/3 left-1/2 -translate-x-1/2 -translate-x-1/2 text-white font-sans text-4xl font-weight: 200'>Software Assignment 2</p>
-        <p id='head2' class='absolute top-1/3 left-1/2 -translate-x-1/2 -translate-x-1/2 text-white font-sans text-4xl font-weight: 200'>Longest Line game 
+        <p id='head1' class='absolute top-1/4 left-1/2 -translate-x-1/2 -translate-x-1/2 text-white font-sans text-4xl font-weight: 200'>Software Assignment 2</p>
+        <p id='head2' class='absolute top-1/4 left-1/2 -translate-x-1/2 -translate-x-1/2 text-white font-sans text-4xl font-weight: 200'>Longest Line game 
         <p> By Vincent Zeng</p>
             </p>
         
-        <button id="myButton" class="" >Play</button>
+        <button id="myButton" class="flex items-center justify-center 
+        absolute border-2 border-white bg-transparent font-sans text-white 
+        w-64 h-12 text-3xl rounded opacity-50 top-96 bottom-0 left-0 right-0 mx-auto 
+        transition duration-300 hover:border-gray-700 hover:bg-gray-200 
+        hover:cursor-pointer hover:text-gray-700 hover:opacity-80 hover:shadow-md" > 
+        Play</button>
+
+        <button id="myButton" class="flex items-center justify-center
+        absolute border-2 border-white bg-transparent font-sans text-white 
+        w-64 h-12 text-3xl rounded opacity-50 top-1/2 bottom-0 left-0 right-0 mx-auto 
+        transition duration-300 hover:border-gray-700 hover:bg-gray-200 
+        hover:cursor-pointer hover:text-gray-700 hover:opacity-80 hover:shadow-md" > 
+        Settings</button>
+
+        <button id="myButton" class="flex items-center justify-center
+        absolute border-2 border-white bg-transparent font-sans text-white 
+        w-64 h-12 text-3xl rounded opacity-50 top-1/2 translate-y-20 bottom-0 left-0 right-0 mx-auto 
+        transition duration-300 hover:border-gray-700 hover:bg-gray-200 
+        hover:cursor-pointer hover:text-gray-700 hover:opacity-80 hover:shadow-md" > 
+        How To Play</button>
         
         <div class='light x1'></div>
         <div class='light x2'></div>
